a6750139080174c93932e8dfe2fc6a9ee31582ef
Christopher Zhong <1946052+chriszhong@users.noreply.github.com>
2018-03-21 19:40:58 +0800

    fix incorrect module

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "creditratingservice.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "credit_report_service.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
