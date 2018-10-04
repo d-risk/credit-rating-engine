cd858b12dd4a762202483efcb6e93e42538eea86
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2018-10-04 18:27:59 +0800

    convert queries of company to use pagination
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

--- company/schema.py
+++ company/schema.py
@@ -1,22 +1,28 @@
+import django_filters
 import graphene
 from graphene_django import DjangoObjectType
+from graphene_django.filter import DjangoFilterConnectionField
 
 import company.models
 
 
 # Annex F - Company Data Service
 # GraphQL data model
-class Company(DjangoObjectType):
+class CompanyFilter(django_filters.FilterSet):
     class Meta:
         model = company.models.Company
+        fields = {
+            'name': ['iexact', 'icontains', 'istartswith'],
+            'industry': ['iexact', 'icontains', 'istartswith'],
+        }
+
+
+class CompanyNode(DjangoObjectType):
+    class Meta:
+        model = company.models.Company
+        interfaces = (graphene.relay.Node,)
 
 
 class CompanyQuery(graphene.ObjectType):
-    company = graphene.Field(Company, id=graphene.UUID())
-    companies = graphene.List(Company, company_name=graphene.String())
-
-    def resolve_company(self, info, id):
-        return company.models.Company.objects.get(id=id)
-
-    def resolve_companies(self, info, company_name):
-        return company.models.Company.objects.filter(name__icontains=company_name)
+    company = graphene.relay.Node.Field(CompanyNode)
+    companies = DjangoFilterConnectionField(CompanyNode, filterset_class=CompanyFilter)
