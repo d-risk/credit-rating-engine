c30cc6785a6362695641ba381a52a75bb54e189d
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2018-10-05 16:29:16 +0800

    use contains filtering for `name` and/or `industry` and show the company's `id` field as the field `company_id` because `id` is masked by the relay Node class
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

--- company/schema.py
+++ company/schema.py
@@ -1,28 +1,34 @@
-import django_filters
-import graphene
+from django_filters import FilterSet
+from graphene.relay import Node
+from graphene.types import ObjectType, UUID
 from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
 
-import company.models
+from company.models import Company
 
 
 # Annex F - Company Data Service
 # GraphQL data model
-class CompanyFilter(django_filters.FilterSet):
+class CompanyFilter(FilterSet):
     class Meta:
-        model = company.models.Company
+        model = Company
         fields = {
-            'name': ['iexact', 'icontains', 'istartswith'],
-            'industry': ['iexact', 'icontains', 'istartswith'],
+            'name': ['icontains', ],
+            'industry': ['icontains', ],
         }
 
 
 class CompanyNode(DjangoObjectType):
+    company_id = UUID(description="The UUID of the company.")
+
     class Meta:
-        model = company.models.Company
-        interfaces = (graphene.relay.Node,)
+        model = Company
+        interfaces = (Node,)
+
+    def resolve_company_id(self, info, **args) -> UUID:
+        return self.id
 
 
-class CompanyQuery(graphene.ObjectType):
-    company = graphene.relay.Node.Field(CompanyNode)
+class CompanyQuery(ObjectType):
+    company = Node.Field(CompanyNode, )
     companies = DjangoFilterConnectionField(CompanyNode, filterset_class=CompanyFilter)
